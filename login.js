62d6900d8f5fbef575c48f617c3e908c01787ba2
Michael <auger.michaell@gmail.com>
2022-11-06 15:18:33 +0100

    commit

--- login.js
+++ login.js
@@ -6,7 +6,7 @@ document.querySelector("#register").addEventListener("click", function () {
     password: document.querySelector("#registerPassword").value,
   };
 
-  fetch("http://localhost:3000/users/signup", {
+  fetch("https://weatherapp-backend-alpha.vercel.app/users/signup", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify(user),
@@ -21,20 +21,20 @@ document.querySelector("#register").addEventListener("click", function () {
 
 // Connection
 document.querySelector("#connection").addEventListener("click", function () {
-    const user = {
-      email: document.querySelector("#connectionEmail").value,
-      password: document.querySelector("#connectionPassword").value,
-    };
+  const user = {
+    email: document.querySelector("#connectionEmail").value,
+    password: document.querySelector("#connectionPassword").value,
+  };
 
-    fetch("http://localhost:3000/users/signin", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(user),
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        if (data.result) {
-          window.location.assign("index.html");
-        }
-      });
-  });
+  fetch("https://weatherapp-backend-alpha.vercel.app/users/signin", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(user),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      if (data.result) {
+        window.location.assign("index.html");
+      }
+    });
+});
